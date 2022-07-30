442120270ab6088cb035fe1f4308377be753c10c
chihungho <a20041060@gmail.com>
2022-07-30 15:17:20 +0800

    add recycler.view

--- app/src/main/java/com/example/foody/models/Result.kt
+++ app/src/main/java/com/example/foody/models/Result.kt
@@ -11,5 +11,27 @@ data class Result(
     @SerializedName("imageType")
     val imageType: String,
     @SerializedName("title")
-    val title: String
+    val title: String,
+    @SerializedName("readyInMinutes")
+    val readyInMinutes: Int,
+    @SerializedName("sourceName")
+    val sourceName: String,
+    @SerializedName("sourceUrl")
+    val sourceUrl: String,
+    @SerializedName("summary")
+    val summary: String,
+    @SerializedName("vegan")
+    val vegan: Boolean,
+    @SerializedName("vegetarian")
+    val vegetarian: Boolean,
+    @SerializedName("veryHealthy")
+    val veryHealthy: Boolean,
+    @SerializedName("aggregateLikes")
+    val aggregateLikes: Int,
+    @SerializedName("cheap")
+    val cheap: Boolean,
+    @SerializedName("dairyFree")
+    val dairyFree: Boolean,
+
+
 )
