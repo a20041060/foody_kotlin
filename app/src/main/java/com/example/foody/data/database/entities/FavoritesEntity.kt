0ab7a12291cdf0515598cc108149cf948ace5fcf
charles <charles.ho@varmeego.com>
2022-08-08 15:14:16 +0800

    Setup room db entities & create functions of insert and delete

---
+++ app/src/main/java/com/example/foody/data/database/entities/FavoritesEntity.kt
@@ -0,0 +1,13 @@
+package com.example.foody.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.foody.models.Result
+import com.example.foody.utils.Constants.Companion.FAVORITE_RECIPES_TABLE
+
+@Entity(tableName= FAVORITE_RECIPES_TABLE)
+class FavoritesEntity (
+    @PrimaryKey(autoGenerate = false)
+    var id: Int,
+    var result: Result
+    )
