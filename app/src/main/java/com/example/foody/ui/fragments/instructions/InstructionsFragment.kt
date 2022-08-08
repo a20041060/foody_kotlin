701291d03f0ebebcff84b03209bd44f4683e3723
charles <charles.ho@varmeego.com>
2022-08-08 14:19:45 +0800

    Modify the InstructionsFragment.kt

--- app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -1,11 +1,16 @@
 package com.example.foody.ui.fragments.instructions
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foody.R
+import com.example.foody.models.Result
+import com.example.foody.utils.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -13,7 +18,20 @@ class InstructionsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val view =  inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        Log.d("websiteUrl",websiteUrl.toString())
+        view.instructions_webView.loadUrl(websiteUrl)
+
+
+        return view
     }
 }
