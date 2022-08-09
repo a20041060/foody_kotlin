a9e849095d926c8094b3661f391401b05748d641
charles <charles.ho@varmeego.com>
2022-08-09 11:12:58 +0800

    Favorite Recipe Recycler and data binding

--- app/src/main/java/com/example/foody/data/database/entities/FavoritesEntity.kt
+++ app/src/main/java/com/example/foody/data/database/entities/FavoritesEntity.kt
@@ -7,7 +7,7 @@ import com.example.foody.utils.Constants.Companion.FAVORITE_RECIPES_TABLE
 
 @Entity(tableName= FAVORITE_RECIPES_TABLE)
 class FavoritesEntity (
-    @PrimaryKey(autoGenerate = false)
+    @PrimaryKey(autoGenerate = true)
     var id: Int,
     var result: Result
     )
